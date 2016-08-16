2039f8ed44a402a662bdfc597b80895080076543
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-08-16 12:51:56 +0200

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,9 @@
 master_doc = 'index'
 
+project = u'SAREhub'
+copyright = u'2016, SARE SA'
+author = u'SARE SA'
+
 # The suffix of source filenames.
 from recommonmark.parser import CommonMarkParser
 
--- docs/index.md
+++ docs/index.md
@@ -1,7 +1,3 @@
-.. toctree::
-   :maxdepth: 2
-   :glob:
-
 ## Wstęp
 
 Ta dokumentacja zawiera opis koncepcji, 

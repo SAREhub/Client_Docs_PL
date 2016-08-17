09b11079838709d876873c01d428e3df9fa680dc
Artur <taniak@op.pl>
2016-08-17 13:10:14 +0200

    version 0.4.2 bumped

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,8 @@ project = u'SAREhub'
 copyright = u'2016, SARE SA'
 author = u'SARE SA'
 
-version = "0.4.1"
-release = "0.4.1"
+version = "0.4.2"
+release = "0.4.2"
 html_show_sphinx = False
 html_style = 'css/extra.css'
 

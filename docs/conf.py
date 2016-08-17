0d3bfc2a6c6bc0c625bc34e75f09a7fe2dbf491d
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-08-17 14:11:06 +0200

    fix config

--- docs/conf.py
+++ docs/conf.py
@@ -6,12 +6,12 @@ author = u'SARE SA'
 
 version = "0.4.2"
 release = "0.4.2"
-html_logo = '/assets/img/logo.png'
-html_favicon = '/assets/img/favicon.ico'
+html_logo = 'assets/img/logo.png'
+html_favicon = 'assets/img/favicon.ico'
 html_show_sphinx = False
 html_style = 'css/extra.css'
 
-latex_logo = '/assets/img/logo.png'
+latex_logo = 'assets/img/logo.png'
 latex_documents = [
   (master_doc, 'index.tex', u'SAREhub',
    u'SARE SA', 'manual', True),

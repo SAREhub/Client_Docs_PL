7607c7a6f7715135bbeac886411e97924e3b4d65
Andrzej Wasiak <mararok@gmail.com>
2016-08-17 15:12:27 +0200

    Styles (#8)
    added logo, and custom css

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,22 @@ author = u'SARE SA'
 
 version = "0.4.2"
 release = "0.4.2"
+html_logo = 'assets/img/logo.svg'
+html_favicon = 'assets/img/favicon.ico'
 html_show_sphinx = False
-html_style = 'css/extra.css'
+html_static_path = [
+'assets/'
+]
 
+html_context = {
+ 'css_files': [
+  'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+  'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+  '_static/css/extra.css',
+ ]
+}
+
+latex_logo = 'assets/img/logo.png'
 latex_documents = [
   (master_doc, 'index.tex', u'SAREhub',
    u'SARE SA', 'manual', True),
